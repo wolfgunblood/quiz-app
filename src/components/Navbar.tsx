450859cf6cdce112a1877a894182211248f5a788
wolfgunblood <jalajdorai1@gmail.com>
2023-09-19 14:12:45 +0530

    Added oAuth functionality

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from 'next/link'
+
+import { getAuthSession } from '@/lib/nextauth'
+import SignInButton from './SignInButton'
+
+type Props = {}
+
+const Navbar = async (props: Props) => {
+    const session = await getAuthSession();
+    console.log(session?.user)
+    return (
+        <div className='fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2'>
+            <div className='flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-full'>
+                <Link href={"/"} className='flex items-center gap-2'>
+                    <p className='rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white'>
+                        Quiz App
+                    </p>
+                </Link>
+
+                <div className='flex items-center'>
+                {session?.user ? (
+                    <p>{session.user.name}</p>
+                ) : (
+                    <SignInButton text="sign in" />
+                )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
