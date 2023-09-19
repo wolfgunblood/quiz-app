bb697a1bed2c15f7639233083dfc8f0b286ed508
wolfgunblood <jalajdorai1@gmail.com>
2023-09-19 12:07:36 +0530

    Connected Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-red-600'>Hello World</h1>
+    <div>
+      <Button>Hello World</Button>
+    </div>
   )
 }
